3436c69373fbe4f31efda2c0632f8131d13c553a
Marco Colombo <mrcojo85@gmail.com>
2014-11-06 01:04:26 +0000

    Interactions and waypoints - first draft

---
+++ Assets/Scripts/Player/Interaction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour {
+
+	public RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 500, 9)) {
+			//Debug.Log (hit.transform.name);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerWayPointController.cs
+++ Assets/Scripts/Player/PlayerWayPointController.cs
@@ -1,76 +1,97 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/*public enum Rotation {
-	Left,
-	Right,
-	None
-} */
-
 public class PlayerWayPointController : MonoBehaviour {
 	public float horizontalBorder;
 	public float speed = 0.0f;
 	public float rotationSpeed = 100.0f;
 	public float maxWalkSpeed = 10.0f;
 	public bool moving;
-	public Transform[] wayPoints;
 
-	private int currentWayPoint;
+	public Interaction interaction;
+	public Material interactiveMaterial;
+
+	public AudioClip footstepSound;
+	public AudioClip interactionSound;
+
+	private Transform facingWayPoint;
 	private bool wayPointReached;
-	private Rotation playerRotation;
 	private Quaternion lastRotation;
 	private Rigidbody rigidBody;
 	private float rangeToStartChecking = 1f;
 	private float dist = 0f;
 	private float lastDist = Mathf.Infinity;
-
+	
 	// Use this for initialization
 	void Start () {
 		moving = false;
-		currentWayPoint = 0;
 		wayPointReached = false;
 		rigidBody = transform.GetComponent<Rigidbody>();
-		playerRotation = Rotation.None;
 		lastRotation = transform.rotation;
 		horizontalBorder = Screen.width / 4f;
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		if (Input.GetMouseButtonUp(0))
+	float fr = 0f;
+	float footsFrequency = 0.7f;
+	void Update()
+	{
+		if( rigidbody.velocity.magnitude > 0f)
 		{
+			fr += Time.deltaTime;
 
-			if ( Input.mousePosition.x >= Screen.width - horizontalBorder )
+			while( fr >= footsFrequency )
 			{
-				playerRotation = Rotation.Right;
-			}       
-			else if ( Input.mousePosition.x <= horizontalBorder )
-			{
-				playerRotation = Rotation.Left;
-			} 
-			else
-			{
-				playerRotation = Rotation.None;
-				Debug.Log("Go for " + (currentWayPoint + 1) + " @ " + maxWalkSpeed);
-				if (currentWayPoint < wayPoints.Length - 1) {
-					wayPointReached = false;
-					Vector3 relativePos = wayPoints[currentWayPoint + 1].position - transform.position;
-					speed = maxWalkSpeed;
-					rigidBody.AddForce(speed * relativePos);
+				fr = 0f;
+				
+				audio.PlayOneShot( footstepSound );
+			}
+		}
+	}
+
+	void FixedUpdate () {
+		if (Input.GetMouseButton(0))
+		{
+			transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+		} 
+		
+		if (Input.GetMouseButton(1))
+		{
+			transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+		} 
+		
+		if (Input.GetMouseButtonUp(2))
+		{
+			if (interaction.hit.transform.tag == "WayPoint" && !moving && !interaction.hit.transform.name.Contains("trigger")) {
+				moving = true;
+				wayPointReached = false;
+				facingWayPoint = interaction.hit.transform;
+				Vector3 relativePos = facingWayPoint.position - transform.position;
+				speed = maxWalkSpeed;
+				rigidBody.AddForce(speed * relativePos);
+			}
+			else if (interaction.hit.transform.tag == "Interactive") {
+				if ((interaction.hit.transform.position - transform.position ).sqrMagnitude < 5) {
+					audio.PlayOneShot( interactionSound);
 				}
-			} 
+			}
 		}
 
-		if (currentWayPoint < wayPoints.Length - 1) {
-			dist = ( wayPoints[currentWayPoint + 1].position - transform.position ).sqrMagnitude;
+		if (interaction.hit.transform.tag == "Interactive") {
+			if ((interaction.hit.transform.position - transform.position ).sqrMagnitude < 5) {
+				interaction.hit.transform.renderer.material = interactiveMaterial;
+			}
 		}
 
+		if (moving) {
+			dist = (facingWayPoint.position - transform.position ).sqrMagnitude;
+		}
+		
 		if (dist < rangeToStartChecking)
 		{
 			if ( dist > lastDist )
 			{
-				currentWayPoint++;
-				Debug.Log(currentWayPoint + " has been reached");
+				moving = false;
 				wayPointReached = true;
 				rigidbody.velocity = Vector3.zero;
 				rigidbody.angularVelocity = Vector3.zero;
@@ -85,29 +106,6 @@ public class PlayerWayPointController : MonoBehaviour {
 		{
 			lastDist = Mathf.Infinity;
 		}
-
-		switch (playerRotation) {
-			case Rotation.None: 
-				break;
-			case Rotation.Left: 
-				if (Vector3.Distance(transform.eulerAngles, (lastRotation * Quaternion.Euler(0, -90, 0)).eulerAngles) > 1f) {
-					transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
-				}
-				else  {
-					lastRotation = transform.rotation;
-					playerRotation = Rotation.None;
-				}
-				
-				break;
-			case Rotation.Right:
-				if (Vector3.Distance(transform.eulerAngles, (lastRotation * Quaternion.Euler(0, 90, 0)).eulerAngles) > 1f) {
-					transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-				}
-				else  {
-					lastRotation = transform.rotation;
-					playerRotation = Rotation.None;
-				}
-				break;
-		} 
+		
 	}
 }
